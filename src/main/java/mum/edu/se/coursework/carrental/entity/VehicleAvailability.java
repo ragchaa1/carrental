3e7e3a237b6b644ec4e5203d12e5e163e4935cbf
ragchaa1 <ragchaa1@gmail.com>
2019-05-17 14:56:52 -0500

    create entity

--- src/main/java/mum/edu/se/coursework/carrental/entity/VehicleAvailability.java
+++ src/main/java/mum/edu/se/coursework/carrental/entity/VehicleAvailability.java
@@ -2,11 +2,14 @@ package mum.edu.se.coursework.carrental.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "VEHICLE_AVAILABILITY")
 @Data
 public class VehicleAvailability {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long vehicleAvaId;
 }
