3ebaf51e6fbc6279fc120118232203584b98e82a
ragchaa1 <ragchaa1@gmail.com>
2019-05-17 14:55:56 -0500

    create entity

--- src/main/java/mum/edu/se/coursework/carrental/entity/Manufacturer.java
+++ src/main/java/mum/edu/se/coursework/carrental/entity/Manufacturer.java
@@ -3,14 +3,16 @@ package mum.edu.se.coursework.carrental.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "MANUFACTURER")
 @Data
 public class Manufacturer {
-
+    @Id
+    @Column(name = "MANUFACTURER_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long mId;
 
 
 }
