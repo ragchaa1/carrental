06299558c5bba079b797d484136782b9cd06bff6
ragchaa1 <ragchaa1@gmail.com>
2019-05-17 15:01:02 -0500

    create entity

--- src/main/java/mum/edu/se/coursework/carrental/repository/VehicleAvailabilityRepository.java
+++ src/main/java/mum/edu/se/coursework/carrental/repository/VehicleAvailabilityRepository.java
@@ -1,6 +1,7 @@
 package mum.edu.se.coursework.carrental.repository;
 
+import mum.edu.se.coursework.carrental.entity.VehicleAvailability;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VehicleAvailabilityRepository extends JpaRepository {
+public interface VehicleAvailabilityRepository extends JpaRepository<VehicleAvailability,Long> {
 }
--- src/main/java/mum/edu/se/coursework/carrental/repository/VehicleRepository.java
+++ src/main/java/mum/edu/se/coursework/carrental/repository/VehicleRepository.java
@@ -1,6 +1,7 @@
 package mum.edu.se.coursework.carrental.repository;
 
+import mum.edu.se.coursework.carrental.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VehicleRepository extends JpaRepository {
+public interface VehicleRepository extends  JpaRepository<Vehicle,Long> {
 }
